50e10cc47d1d850a78ba0d2af5d73282075a7aed
esemennikov <esemennikov@openprovider.ru>
2016-08-31 12:53:38 +0200

    AZ-1455, ATF correction

--- tests/WHMCSModuleTest.php
+++ tests/WHMCSModuleTest.php
@@ -77,4 +77,60 @@ class WHMCSModuleTest extends PHPUnit_Framework_TestCase
             }
         }
     }
+
+    public function testAPI()
+    {
+	// The fully qualified URL to your WHMCS installation root directory
+	$whmcsUrl = "http://whmcs.fgershunov.openprovider.nl/";
+	 
+	// Admin username and password
+	$username = "root";
+	$password = "masterkey";
+	 
+	// Set post values
+	$postfields = array(
+	    'username' => $username,
+	    'password' => md5($password),
+	    'action' => 'GetOrders',
+	    'responsetype' => 'json',
+	    'id' => 403,
+	);
+	 
+	// Call the API
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $whmcsUrl . 'includes/api.php');
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postfields));
+	$response = curl_exec($ch);
+	if (curl_error($ch)) {
+	    die('Unable to connect: ' . curl_errno($ch) . ' - ' . curl_error($ch));
+	}
+	curl_close($ch);
+	 
+	// Attempt to decode response as json
+	$jsonData = json_decode($response, true);
+	 
+	// Dump array structure for inspection
+	var_dump($jsonData);
+
+	$this->assertNotNull($jsonData);	
+    }
+    
+    public function testOpApi()
+    {
+	$params = [
+	    'OpenproviderAPI' => 'https://api.cte.openprovider.eu/',
+	    'Username' => 'opdrs4',
+	    'Password' => 'opdrs4',
+	];
+
+	$products = opApiWrapper::processRequest('searchProductSslCertRequest', $params, []);
+	
+	error_log(var_export($products,true));
+
+	$this->assertNotNull($products);
+    } 
 }
+
--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -8,6 +8,7 @@ if (!defined('WHMCS')) {
 
 // Include the WHMCS module.
 require_once __DIR__ . '/../modules/servers/provisioningmodule/provisioningmodule.php';
+require_once __DIR__ . '/../modules/servers/openprovidersslnew/lib/opApiWrapper.php';
 
 /**
  * Mock logModuleCall function for testing purposes.
