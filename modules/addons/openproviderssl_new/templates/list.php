05b172c6274e439c3c2ae208a6716700871964f7
Eugene Semennikov <turboasm@mail.ru>
2016-09-05 15:54:57 +0700

    AZ-1457, proto

--- modules/addons/openproviderssl_new/templates/list.php
+++ modules/addons/openproviderssl_new/templates/list.php
@@ -1,4 +1,8 @@
 <?php if (!empty($view['products'])): ?>
+    <?php echo $view['errorMessage']; ?>
+<?php endif; ?>
+
+<?php if (!empty($view['products'])): ?>
     <table width="100%" cellspacing="1" cellpadding="3" border="0" class="datatable">
         <tbody>
         <tr>
@@ -6,16 +10,20 @@
             <th><?php echo 'name'; ?></th>
             <th><?php echo 'brandName'; ?></th>
             <th><?php echo 'price'; ?></th>
+            <th><?php echo 'currency'; ?></th>
         </tr>
         <?php foreach ($view['products'] as $item): ?>
             <tr>
                 <td><?php echo $item['id']?></td>
                 <td><?php echo $item['name']?></td>
                 <td><?php echo $item['brandName']?></td>
-                <td><?php echo $item['price']?></td>
+                <td><?php echo $item['warranty']['reseller']['price']?></td>
+                <td><?php echo $item['warranty']['reseller']['currency']?></td>
             </tr>
         <?php endforeach; ?>
         </tbody>
     </table>
 
 <?php endif; ?>
+
+<a href="<?php echo $view['global']['mod_url'] ?>&action=list"><?php echo 'Back'; ?></a>
